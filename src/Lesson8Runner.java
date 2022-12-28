987fceabd3456a47d76fcf4fd55a8185af8bf011
dmatveyenka <dmdev2020@gmail.com>
2022-12-28 09:41:40 +0100

    lesson-8

---
+++ src/Lesson8Runner.java
@@ -0,0 +1,31 @@
+public class Lesson8Runner {
+
+    public static void main(String[] args) {
+        double doubleValue = 12.98; // define + init
+        System.out.println(doubleValue);
+
+        int intValue; // define
+        intValue = 28; // initialization
+        System.out.println(intValue); // int
+
+        intValue = 67;
+        System.out.println(intValue);
+
+        int intValue2 = 1908;
+        intValue = intValue2;
+        System.out.println(intValue);
+
+        final byte byteValue = 39;
+        System.out.println(byteValue);
+//        byteValue = -89;
+
+        char charValue = 'q';
+        System.out.println(charValue);
+
+        boolean booleanValue = true;
+        System.out.println(booleanValue);
+
+        String stringValue = "Hello World";
+        System.out.println(stringValue);
+    }
+}
