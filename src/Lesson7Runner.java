82d4ea197770c841a8fd8427f4de8f225e7e610c
dmatveyenka <dmdev2020@gmail.com>
2022-12-28 09:22:34 +0100

    lesson-7

---
+++ src/Lesson7Runner.java
@@ -0,0 +1,15 @@
+public class Lesson7Runner {
+
+    public static void main(String[] args) {
+        System.out.println(25); // int
+        System.out.println(25L); // long
+
+        System.out.println(23.879); // double
+        System.out.println(23.879F); // float
+
+        System.out.println('?'); // char
+        System.out.println("?,."); // String
+
+        System.out.println(false); // boolean
+    }
+}
