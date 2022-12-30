93817fa173e514b16153ccb711d986af2ad941be
dmatveyenka <dmdev2020@gmail.com>
2022-12-30 09:25:09 +0100

    lesson-9

---
+++ src/Lesson9Runner.java
@@ -0,0 +1,24 @@
+public class Lesson9Runner {
+
+    public static void main(String[] args) {
+        int value = 10;
+        System.out.println(value / (double) 4);
+
+//        int value = 10;
+//        value = value + 1; // 1
+//        value += 1; // 2
+//        value++; // 3
+//        value--; // value - 1; value -= 1;
+//        System.out.println(value);
+
+//        int value = 10;
+//        value = (value + 5) / 3;
+//        System.out.println(value);
+
+//        +, -, *, /, %
+//        int first = 10;
+//        int second = 4;
+//        int value = first % second;
+//        System.out.println(value);
+    }
+}
