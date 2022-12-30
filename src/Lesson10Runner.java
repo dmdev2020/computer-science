b20568460d82b18f7af5a628d816972207ecd5d4
dmatveyenka <dmdev2020@gmail.com>
2022-12-30 09:39:49 +0100

    lesson-10

---
+++ src/Lesson10Runner.java
@@ -0,0 +1,17 @@
+public class Lesson10Runner {
+
+    public static void main(String[] args) {
+//       0 < x <= 100
+        int x = 167;
+        boolean result = 0 < x && x <= 100;
+        System.out.println(!result);
+
+//      <, >, ==, <=, >=, !=
+//        boolean value = 10 > 5;
+//        System.out.println(value); // true
+//        System.out.println(10 > 20); // false
+//        System.out.println(7 == 6); // false
+//        System.out.println(5 >= 4); // true
+//        System.out.println(90 != 90); // false
+    }
+}
